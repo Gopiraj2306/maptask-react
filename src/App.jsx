502c5c8de48cb1896fdbd845d8e0f8f3b23d7a44
gopiraj2306 <gopi.venkatesann@gmail.com>
2025-07-25 15:17:29 +0530

    add para

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import WorldMap from "./components/WorldMap";
 import ImageCardContainer from "./components/testing";
 import SearchAppBar from "./components/material";
+import Para from "./components/para";
 function App() {
   return (
     <div className="app-container">
       <SearchAppBar />
       <WorldMap />
       <ImageCardContainer />
+      <Para />
     </div>
   );
 }
---
+++ src/components/para.jsx
@@ -0,0 +1,8 @@
+function Para(){
+    return(
+        <div>
+            <p>how to resolve merge conflict error</p>
+        </div>
+    )
+}
+export default Para
